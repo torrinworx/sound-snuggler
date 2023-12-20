3f0bb15b18469c718fce9bd929432bc25eb83081
torrin1 <torrin@worx4you.com>
2023-12-19 20:29:37 -0500

    Making lyrics auto fetch

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-Run with:
+# SoundSnuggler
+
+<p align="center">
+  <img src="/assets/sound_snuggler_logo.png" alt="SoundSnuggler Logo" width="50%">
+</p>
+
+SoundSnuggler is an innovative tool designed to enhance your music experience. It completes your MP3 files by automatically fetching animated album art from Apple Music, searching for song lyrics online, and synchronizing them with audio using advanced AI. This process creates enriched files with embedded Enhanced LRC (ELRC), offering a more immersive listening experience.
+
+## Features
+
+- **Automated Album Art Downloading:** Fetches and embeds animated album art from Apple Music into your MP3 files.
+- **Lyrics Search & Synchronization:** Scours the internet for song lyrics and uses AI to synchronize these lyrics with your music tracks.
+- **ELRC Embedding:** Enhances your music files with ELRC, allowing for synchronized lyric display as the song plays.
+
+## Enhanced LRC Format (A2 Extension)
+
+SoundSnuggler utilizes an advanced version of the Simple LRC Format, known as the Enhanced LRC Format (A2 Extension). This extension differs in two significant ways:
+
+1. **Line Timestamps:** These indicate the end of the previous word, enabling LRC players to determine the duration of each word and the timing for the next line display.
+2. **Word Time Tag Format:** Incorporates tags in the `<mm:ss.xx>` format for precise synchronization.
+
+## Getting Started
+
+To run SoundSnuggler, follow these simple steps:
+
+```bash
+pipenv install
+pipenv shell
+
+pipenv run python ./main.py
+
+or
+
+py ./main.py
 ```
-pipenv run python .\main.py
-```
+
+## Roadmap (TODO)
+
+- **Enhanced Transcription Accuracy:** Investigate [stable-ts](https://github.com/jianfch/stable-ts) for improved timestamped transcription in ELRC lyrics.
+- **Speaker Diarization Integration:** Explore combining Whisper's capabilities with speaker diarization tools for complex audio files. This integration would help in identifying not only the timing of the words but also the speaker, particularly in songs with multiple vocalists. More details can be found in this [discussion on Hugging Face](https://huggingface.co/spaces/openai/whisper/discussions/29).
+- **Animated Album Art Integration:** Implement functionality to use the [Apple Music Animated Artwork Fetcher](https://github.com/bunnykek/Apple-Music-Animated-Artwork-Fetcher) for retrieving and embedding animated album art into songs.
--- modules/lyrics_synchronizer.py
+++ modules/lyrics_synchronizer.py
@@ -1,109 +1,91 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox, scrolledtext
 from PIL import Image, ImageTk
+import syncedlyrics
 from .media_info_handler import MediaInfoHandler
 
 class LyricsSynchronizer(tk.Frame):
-    # Generating out lyrics and syncing lyrics for a song
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         self.parent = parent
         self.album_art_label = None
-        self.max_image_size = (200, 200)  # Maximum size for the album art
-        self.default_album_art = Image.new('RGB', self.max_image_size, color = 'grey')  # Default album art
+        self.max_image_size = (200, 200)
+        self.default_album_art = Image.new('RGB', self.max_image_size, color='grey')
 
-        self.album_info_var = tk.StringVar()  # To store album info
-        self.lyric_file_info_var = tk.StringVar()  # To store lyric file info
-        self.download_path_info_var = tk.StringVar()  # To store download path info
+        self.album_info_var = tk.StringVar()
+        self.download_path_info_var = tk.StringVar()
 
         self.create_widgets()
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(1, weight=1)
 
     def create_widgets(self):
-        # Album art label
-        self.display_album_art(self.default_album_art)  # Display default album art initially
+        self.album_art_frame = tk.Frame(self)
+        self.album_art_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nw")
 
-        # Lyrics text area
+        self.display_album_art(self.default_album_art, self.album_art_frame)
+
+        control_frame = tk.Frame(self)
+        control_frame.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+        control_frame.grid_columnconfigure(0, weight=1)
+
+        upload_button_frame = tk.Frame(control_frame)
+        upload_button_frame.grid(row=0, column=0, sticky="w")
+
+        self.upload_music_button = tk.Button(upload_button_frame, text="Upload Music File", command=self.upload_music_file)
+        self.upload_music_button.grid(row=0, column=0, padx=5, pady=5)
+
+        self.set_download_path_button = tk.Button(upload_button_frame, text="Set Download Path", command=self.set_download_path)
+        self.set_download_path_button.grid(row=1, column=0, padx=5, pady=5)
+
+        info_label_frame = tk.Frame(control_frame)
+        info_label_frame.grid(row=1, column=0, sticky="ew")
+
+        tk.Label(info_label_frame, textvariable=self.album_info_var).grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        tk.Label(info_label_frame, textvariable=self.download_path_info_var).grid(row=1, column=0, padx=5, pady=5, sticky="w")
+
         self.lyrics_text = scrolledtext.ScrolledText(self, wrap=tk.WORD)
-        self.lyrics_text.grid(row=1, column=1, sticky="nsew")
+        self.lyrics_text.grid(row=0, column=1, rowspan=2, padx=10, pady=10, sticky="nsew")
 
-        # Label to display album info
-        tk.Label(self, textvariable=self.album_info_var).grid(row=2, column=0, sticky="w")
-
-        # Label to display lyric file info
-        tk.Label(self, textvariable=self.lyric_file_info_var).grid(row=2, column=1, sticky="w")
-
-        # Label to display download path info
-        tk.Label(self, textvariable=self.download_path_info_var).grid(row=2, column=2, sticky="w")
-
-        # Buttons for uploading music and lyrics files
-        self.upload_music_button = tk.Button(self, text="Upload Music File", command=self.upload_music_file)
-        self.upload_music_button.grid(row=0, column=0, padx=10, pady=10)
-
-        self.upload_lyrics_button = tk.Button(self, text="Upload Lyrics File", command=self.upload_lyrics_file)
-        self.upload_lyrics_button.grid(row=0, column=1, padx=10, pady=10)
-
-        # Button to set download path
-        self.set_download_path_button = tk.Button(self, text="Set Download Path", command=self.set_download_path)
-        self.set_download_path_button.grid(row=0, column=2, padx=10, pady=10)
-
-
-    def upload_music_file(self):
-        # Logic to upload and handle music files (MP3 and FLAC)
-        file_path = filedialog.askopenfilename(filetypes=[("MP3 Files", "*.mp3"), ("FLAC Files", "*.flac")])
-        if file_path:
-            song_name, album_art_image = MediaInfoHandler.get_song_info(file_path)
-            self.album_info_var.set(f"Song: {song_name}")
-            if album_art_image:
-                self.display_album_art(album_art_image)
-
-            # Handle synchronized lyrics if available
-            synchronized_lyrics = MediaInfoHandler.extract_synchronized_lyrics(file_path)
-            if synchronized_lyrics:
-                self.display_synchronized_lyrics(synchronized_lyrics)
-
-    def display_album_art(self, image):
+    def display_album_art(self, image, parent_frame):
         if self.album_art_label:
             self.album_art_label.destroy()
 
-        # Resize while maintaining aspect ratio
         image.thumbnail(self.max_image_size, Image.Resampling.LANCZOS)
-
         photo = ImageTk.PhotoImage(image)
-        self.album_art_label = tk.Label(self, image=photo)
-        self.album_art_label.image = photo  # keep a reference!
-        self.album_art_label.grid(row=1, column=0, padx=10, pady=10)
+        self.album_art_label = tk.Label(parent_frame, image=photo)
+        self.album_art_label.image = photo
+        self.album_art_label.pack()
 
-    def display_synchronized_lyrics(self, lyrics):
-        # Logic to display the synchronized lyrics
-        lyrics_text = '\n'.join([f"{time}: {text}" for time, text in lyrics])
-        self.lyrics_text.delete('1.0', tk.END)
-        self.lyrics_text.insert(tk.END, lyrics_text)
-
-    def upload_lyrics_file(self):
-        file_path = filedialog.askopenfilename(filetypes=[("LRC Files", "*.lrc")])
+    def upload_music_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("MP3 Files", "*.mp3"), ("FLAC Files", "*.flac")])
         if file_path:
-            self.lyric_file_info_var.set(f"Lyric File: {file_path}")  # Update lyric file info label
-            self.load_lyrics_file(file_path)
+            try:
+                song_name, album_art_image = MediaInfoHandler.get_song_info(file_path)
+                self.album_info_var.set(f"Song: {song_name}")
+                if album_art_image:
+                    self.display_album_art(album_art_image, self.album_art_frame)
+
+                self.fetch_lyrics(song_name, '')  # Assuming artist name is not available
+            except Exception as e:
+                messagebox.showerror("Error", f"An error occurred while processing the music file: {e}")
+
+    def fetch_lyrics(self, track_name, artist_name):
+        try:
+            lrc = syncedlyrics.search(f"{track_name} {artist_name}")
+            if lrc:
+                self.display_lyrics(lrc)
+            else:
+                messagebox.showinfo("Info", "Lyrics not found.")
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {e}")
 
     def set_download_path(self):
         download_path = filedialog.askdirectory()
         if download_path:
-            self.download_path_info_var.set(f"Download Path: {download_path}")  # Update download path info label
-
-
-    def load_lyrics_file(self, file_path):
-        # Logic to load and parse the lyrics file
-        try:
-            with open(file_path, 'r') as file:
-                lyrics_content = file.read()
-                self.display_lyrics(lyrics_content)
-        except IOError as e:
-            messagebox.showerror("Error", f"Error opening file: {e}")
+            self.download_path_info_var.set(f"Download Path: {download_path}")
 
     def display_lyrics(self, lyrics):
-        # Logic to display the lyrics
         self.lyrics_text.delete('1.0', tk.END)
         self.lyrics_text.insert(tk.END, lyrics)
 
