7c995b697b6161ea4aa6a40d21785f8a2aa99363
torrin1 <torrin@worx4you.com>
2023-12-27 21:29:02 -0500

    Updating muisc player to display synced lyrics live

--- modules/music_player.py
+++ modules/music_player.py
@@ -47,6 +47,10 @@ class MusicPlayer(ttk.Frame):
         self.lyrics_display = scrolledtext.ScrolledText(self, wrap=tk.WORD, width=40, height=10)
         self.lyrics_display.pack(pady=10)
 
+        # Add this line to create the synchronized lyrics display
+        self.sync_lyrics_display = ttk.Label(self, text="", wraplength=300)
+        self.sync_lyrics_display.pack(pady=10)
+
     # Media Handling
     def load_song(self):
         song_path = filedialog.askopenfilename(filetypes=[("Audio Files", "*.mp3 *.flac")])
@@ -65,12 +69,6 @@ class MusicPlayer(ttk.Frame):
             self.song_info.config(text=f"Song: {song_name}")
             self.update_album_art_ui(album_art_image)
             self.update_lyrics_display()
-        
-        # Extract and store synchronized lyrics
-        self.synchronized_lyrics = MediaInfoHandler.extract_synchronized_lyrics(self.current_song)
-        
-        # Update the initial lyrics display
-        self.update_lyrics_display()
 
     def update_album_art_ui(self, album_art_image):
         self.album_cover_label.image = None
@@ -84,19 +82,68 @@ class MusicPlayer(ttk.Frame):
         lyrics = LyricsHandler.retrieve_lyrics_from_file(self.current_song)
         self.lyrics_display.delete(1.0, tk.END)
         self.lyrics_display.insert(tk.END, lyrics["unsynced_lyrics"] if lyrics["unsynced_lyrics"] else "Lyrics not available.")
+        print(lyrics["synced_lyrics"])
+        self.synced_lyrics = self.parse_synced_lyrics(lyrics["synced_lyrics"])
+        print("Synced Lyrics Parsed:", self.synced_lyrics)  # Debugging print
+
+    def parse_synced_lyrics(self, synced_lyrics_raw):
+        parsed_synced_lyrics = []
+        for line in synced_lyrics_raw.split('\n'):
+            # Debugging print to see each line
+            print("Line:", line)
+
+            # Splitting the line into timestamp and text
+            if ']: ' in line:
+                timestamp_str, text = line.split(']: ', 1)
+                timestamp_str = timestamp_str.strip('[]ms')
+                # Debugging print to check timestamp string
+                print("Timestamp str:", timestamp_str)
+
+                if timestamp_str.isdigit():
+                    time_ms = int(timestamp_str)
+                    parsed_synced_lyrics.append((time_ms, text))
+                else:
+                    # Debugging print in case of parsing issue
+                    print("Timestamp not a digit:", timestamp_str)
+            else:
+                # Debugging print in case line format is not as expected
+                print("Incorrect line format:", line)
+
+        return parsed_synced_lyrics
+
+
+
+    def update_synced_lyrics_display(self, current_time):
+        if not hasattr(self, 'synced_lyrics') or not self.synced_lyrics:
+            self.sync_lyrics_display.config(text="")
+            return
+
+        current_text = ""
+        for time, text in self.synced_lyrics:
+            if current_time >= time:
+                current_text = text
+            else:
+                break
+        self.sync_lyrics_display.config(text=current_text)
+        print("Current Synced Lyric:", current_text)  # Debugging print
 
     def update_progress(self):
-        if self.current_song:
-            try:
-                current_time = self.player.get_time() / 1000
-                total_time = self.time_slider.cget("to")
-                self.time_slider.set(current_time)
-                self.time_info.config(text=f"{self.format_time(current_time)} / {self.format_time(total_time)}")
-                self.parent.update_idletasks()
-                if self.is_playing:
-                    self.after(1000, self.update_progress)
-            except Exception as e:
-                print(f"Error in update_progress: {e}")
+        if not self.current_song:
+            return
+
+        try:
+            current_time = self.player.get_time()
+            print("Current Time:", current_time)  # Debugging print
+
+            total_time = self.time_slider.cget("to") * 1000
+            self.time_slider.set(current_time / 1000)
+            self.time_info.config(text=f"{self.format_time(current_time / 1000)} / {self.format_time(total_time / 1000)}")
+            self.update_synced_lyrics_display(current_time)
+            self.parent.update_idletasks()
+            if self.is_playing:
+                self.after(100, self.update_progress)
+        except Exception as e:
+            print(f"Error in update_progress: {e}")
 
     # Player Control Methods
     def toggle_play_pause(self):
